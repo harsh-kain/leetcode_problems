7e5604a24e6c9bf916c490e16c36b12efa94f64d
Harsh Kain <72141797+harsh-kain@users.noreply.github.com>
2023-06-16 17:38:24 +0000

    adding shuffle the array

---
+++ shuffleTheArray.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+         int i=0,j=n;
+        vector<int> res;
+        while(j<2*n){
+            res.push_back(nums[i]);
+            res.push_back(nums[j]);
+            i++;
+            j++;
+        }
+        return res;
+        
+    }
+};
+int main()
+{
+    
+    return 0;
+}
