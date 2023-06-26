25d15a8f4481e4b36de41192c175894d4d0c8e40
Harsh Kain <72141797+harsh-kain@users.noreply.github.com>
2023-06-26 15:49:11 +0000

    adding valid anagram file

---
+++ validAnagram.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+bool isAnagram(string s, string t) {
+    if(s.size() != t.size()){
+        return false;
+    }
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+
+    for(int i =0; i<s.length(); i++){
+        if(s[i] != t[i]){
+            return false;
+        }
+        
+    } 
+    return true;
+}
+int main()
+{
+    return 0;
+}
