37323650e77b15dc115587c3a272b1ad31edc8c4
Harsh Kain <72141797+harsh-kain@users.noreply.github.com>
2023-07-02 15:34:40 +0000

    adding remove deplicate form sorted array

---
+++ removeDuplicateFromSortedArray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int removeDuplicates(vector<int>& nums) {
+    int j = 1;
+    for(int i = 1; i < nums.size(); i++){
+        if(nums[i] != nums[i - 1]){
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    return j;
+}
+int main()
+{
+    return 0;
+}
