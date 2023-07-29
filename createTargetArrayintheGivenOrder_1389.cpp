c20e69556efaace988bcfff03408def42f0d540d
Harsh Kain <72141797+harsh-kain@users.noreply.github.com>
2023-07-29 17:58:07 +0000

    adding create target array in the given order file

---
+++ createTargetArrayintheGivenOrder_1389.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index){
+        
+     int n=nums.size();
+     vector<int> ans;
+     for(int i=0;i<n;i++)
+        ans.insert(ans.begin()+index[i],nums[i]);
+    return ans;
+}
+};
+
+int main()
+{
+    return 0;
+}
