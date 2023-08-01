93e8e15a30edee7af44c3e712042be3d60555ef8
Harsh Kain <72141797+harsh-kain@users.noreply.github.com>
2023-08-01 17:30:31 +0000

    adding rotate image file

---
+++ rotateImage_42.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin() , matrix.end());
+        int row = matrix.size();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j <= i; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+    }
+};
+int main()
+{
+    return 0;
+}
